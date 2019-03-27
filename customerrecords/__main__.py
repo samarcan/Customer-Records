4bf2cf3513334014460f1f8b696b37a1f37b266e
samarcan <samuel.martin.cantalejo@gmail.com>
2019-03-27 22:24:14 +0100

    Add possibility of change km filter and data filte path

--- customerrecords/__main__.py
+++ customerrecords/__main__.py
@@ -2,6 +2,10 @@ from models import Customer, Location, Headquarter, Party
 from utils.datareader import DataReader
 
 
+DEFAULT_KM_FILTER = 100.0
+DEFAULT_FILE_PATH = "assets/customers.txt"
+
+
 def createCustomer(data):
     latitude = data.get('latitude', '')
     longitude = data.get('longitude', '')
@@ -16,13 +20,29 @@ def printPartyGuests(party):
         print("Id: {id}\tName: {name}".format(id=guest.id, name=guest.name))
 
 
+def receiveInput():
+    fileDataPath = input(
+        "Insert path of customer data "
+        "file (default='%s'): " % DEFAULT_FILE_PATH)
+    kmFilter = input(
+        "Insert radius in km where "
+        "find the guests (default=%.2f): " % DEFAULT_KM_FILTER)
+    kmFilter = float(kmFilter) if kmFilter != '' else DEFAULT_KM_FILTER
+    fileDataPath = fileDataPath if fileDataPath != '' else DEFAULT_FILE_PATH
+    return kmFilter, fileDataPath
+
+
 if __name__ == '__main__':
-    hqLocation = Location(53.339428, -6.257664)
-    headquarter = Headquarter('Intercom Dublin', hqLocation)
-    party = Party(headquarter, 100)
-    dataReader = DataReader('./assets/customers.txt')
-    for data in dataReader.readData():
-        customer = createCustomer(data)
-        party.addGuest(customer)
-    party.sortGuests('id', 'ASC')
-    printPartyGuests(party)
+    try:
+        kmFilter, fileDataPath = receiveInput()
+        hqLocation = Location(53.339428, -6.257664)
+        headquarter = Headquarter('Intercom Dublin', hqLocation)
+        party = Party(headquarter, kmFilter)
+        dataReader = DataReader(fileDataPath)
+        for data in dataReader.readData():
+            customer = createCustomer(data)
+            party.addGuest(customer)
+        party.sortGuests('id', 'ASC')
+        printPartyGuests(party)
+    except Exception:
+        print("Program has finished due an error.")
